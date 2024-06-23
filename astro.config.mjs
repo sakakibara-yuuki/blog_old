c45f73b82f38a573d0b897daf7d41f1e7371c3d8
sakakibara yuuki <sakakilabo0000@gmail.com>
2024-06-23 22:24:55 +0900

    feat: intoroduce d2 test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import d2 from "astro-d2";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sakakibara-yuuki.github.io',
-    output: 'static',
-	integrations: [mdx(), sitemap()],
+  site: 'https://sakakibara-yuuki.github.io',
+  output: 'static',
+  integrations: [mdx(), sitemap(), d2()]
 });
