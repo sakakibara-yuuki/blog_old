0dd1e3dd5359911bbb29040cf326d66586938549
sakakibara yuuki <sakakilabo0000@gmail.com>
2024-06-23 19:36:48 +0900

    fix: fix output values for gh pages. gh pages is static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://sakakibara-yuuki.github.io',
     base: '/blog',
+    output: 'static',
 	integrations: [mdx(), sitemap()],
 });
