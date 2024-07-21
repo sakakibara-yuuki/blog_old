0d5bb934a1c66654a5a9c3f6a66dda8e435b2dcd
sakakibara yuuki <sakakilabo0000@gmail.com>
2024-07-21 15:37:36 +0900

    chore: add Contents tree

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import d2 from "astro-d2";
+import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sakakibara-yuuki.github.io',
   output: 'static',
-  integrations: [mdx(), sitemap(), d2()]
+  integrations: [mdx(), sitemap(), d2()],
+  markdown: {
+    remarkPlugins: [ [remarkToc, { heading: "Contents" }] ],
+  }
 });
