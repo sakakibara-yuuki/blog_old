6431c95e79f941fce4ca365fd99853d3fac8413f
sakakibara yuuki <sakakilabo0000@gmail.com>
2024-09-06 00:42:35 +0900

    feat: add change style and add dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,13 @@ import rehypeKatex from "rehype-katex";
 
 import icon from "astro-icon";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sakakibara-yuuki.github.io',
   output: 'static',
-  integrations: [mdx(), sitemap(), d2(), icon()],
+  integrations: [mdx(), sitemap(), d2(), icon(), preact()],
   markdown: {
     remarkPlugins: [ remarkMath, [remarkToc, { heading: "Contents" }] ],
     rehypePlugins: [ rehypeKatex ],
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,19 @@
+/*
+ * Greeting.jsx
+ * Copyright (C) 2024 sakakibara <sakakibara@organon>
+ *
+ * Distributed under terms of the MIT license.
+ */
+import { useState } from 'preact/hooks';
+
+
+export default function Greeting({messages}) {
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+  const [greeting, setGreeting] = useState(randomMessage());
+
+  return (
+    <div>
+      <h3>{greeting}</h3>
+    </div>
+  );
+}
